743e31a19e0b757425cc5d9c45093a20e28b23a5
Juan Polanco <juanpolanco5008@gmail.com>
2020-03-06 23:07:24 -0500

    Update script.js
    Added comments.

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 
+// Created variables for the users input, button and the UL
 var input = document.getElementById("userinput");
 var button = document.getElementById("enter");
 var ul = document.querySelector("ul");
 
+
+/// Here I created a function to get the length of the users input.
 function inputLength() {
 	return input.value.length;
 }
 
+/// Here I create a function to add the users input to the UL 
 function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = " ";
 }
-
+/// This function listens for a user click and checks to see if 
+/// the users input is greater than 0 and if so it will create the element
 function addListAfterClick() {
 	if (inputLength() > 0 ){
 	createListElement();
 }
 }
+/// creatin a function for keypress same as above.
 
 function addlistAfterKeypress(event) {
 	if (inputLength > 0 && event.keyCode === 13 ){
 	createListElement();
 }
 }
+
+/// below, are the event listers for both click and key press
 
 button.addEventListener("click", addListAfterClick)
 
